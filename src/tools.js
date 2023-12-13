188af4860dbf4cf0358b0cf4de684111edf21903
wangge-star <790550927@qq.com>
2023-12-13 17:36:02 +0800

    17:35

---
+++ src/tools.js
@@ -0,0 +1,44 @@
+import fa_comment from "@fortawesome/fontawesome-free/svgs/solid/comment.svg";
+import fa_paper_plane from "@fortawesome/fontawesome-free/svgs/solid/paper-plane.svg";
+import fa_user_circle from "@fortawesome/fontawesome-free/svgs/solid/circle-user.svg";
+import fa_street_view from "@fortawesome/fontawesome-free/svgs/solid/street-view.svg";
+import fa_camera_retro from "@fortawesome/fontawesome-free/svgs/solid/camera-retro.svg";
+import fa_info_circle from "@fortawesome/fontawesome-free/svgs/solid/circle-info.svg";
+import fa_xmark from "@fortawesome/fontawesome-free/svgs/solid/xmark.svg";
+
+import showMessage from "./message.js";
+
+const tools = {
+    "switch-model": {
+        icon: fa_user_circle,
+        callback: () => {}
+    },
+    "photo": {
+        icon: fa_camera_retro,
+        callback: () => {
+            showMessage("照好了嘛，是不是很可爱呢？", 6000, 9);
+            Live2D.captureName = "photo.png";
+            Live2D.captureFrame = true;
+        }
+    },
+    "info": {
+        icon: fa_info_circle,
+        callback: () => {
+            open("https://www.baidu.com");
+        }
+    },
+    "quit": {
+        icon: fa_xmark,
+        callback: () => {
+            localStorage.setItem("waifu-display", Date.now());
+            showMessage("最难的是相遇", 2000, 11);
+            document.getElementById("waifu").style.bottom = "-500px";
+            setTimeout(() => {
+                document.getElementById("waifu").style.display = "none";
+                document.getElementById("waifu-toggle").classList.add("waifu-toggle-active");
+            }, 3000);
+        }
+    }
+};
+
+export default tools;
